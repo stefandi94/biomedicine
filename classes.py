f189319c7d2d72dd672a964350e0f4c8e15fa0ef
Milan <milan.ignjic93@gmail.com>
2019-05-28 20:00:29 +0200

    added funcionality to update coordinates and to plot bones and joints

--- classes.py
+++ classes.py
@@ -1,8 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class Point:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
 
     def toString(self):
         print('(x, y) = ({}, {})'.format(self.x, self.y))
@@ -11,17 +21,40 @@ class Point:
 class Bone:
 
     def __init__(self, points):
-        self.points = points
+        n = len(points)
+        self.matrix = np.zeros((n, 2))
+
+        for i, point in enumerate(points):
+            self.matrix[i] = np.array([point.getX(), point.getY()])
+
+    def update_points(self, matrix):
+        self.matrix = matrix
+
+    def to_vector(self):
+        m, n = self.matrix.shape
+        row_vector = np.reshape(self.matrix, m*n)
+        return row_vector
+
+    def plot(self):
+        plt.plot(self.matrix[:, 0], self.matrix[:, 1])
 
     def toString(self):
-        for point in self.points:
-            point.toString()
+        for row in self.matrix:
+            print('(x, y) = ({}, {})'.format(row[0], row[1]))
 
 class Joint:
 
     def __init__(self, upper_bone, lower_bone):
         self.upper_bone = upper_bone
         self.lower_bone = lower_bone
+
+    def update_bones(self, new_upper_bone, new_lower_bone):
+        self.upper_bone.update_points(new_upper_bone)
+        self.lower_bone.update_points(new_lower_bone)
+
+    def plot(self):
+        self.upper_bone.plot()
+        self.lower_bone.plot()
 
     def toString(self):
         print('Upper bone:')
